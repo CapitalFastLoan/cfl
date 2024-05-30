75bc5dfc245991d54f3e74e62c59430d320211e4
Manish Jangir <manish.jangir@eshiksa.in>
2024-05-30 11:20:09 +0530

    Initial commit

---
+++ models/User.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const UserSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    dob: {
+        type: Date,
+        required: true,
+    },
+    father_name: {
+        type: String,
+        required: true,
+    },
+    marital_status: {
+        type: String,
+        enum: ["single", "married"],
+        required: true,
+    },
+    qualification_status: {
+        type: String,
+        enum: ["graduate", "non graduate"],
+        required: true,
+    },
+    occupation_status: {
+        type: String,
+        enum: ["business", "salaried"],
+        required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+      required: false,
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin","employee"],
+      default: "customer",
+    },
+    hash: String,
+    salt: String,
+  },
+  { timestamps: true }
+);
+
+UserSchema.methods.setPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString("hex");
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, `sha512`)
+    .toString("hex");
+};
+
+UserSchema.methods.validatePassword = function (password) {
+  var hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, `sha512`)
+    .toString(`hex`);
+  return this.hash === hash;
+};
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express"),
+  app = express();
+require("dotenv").config();
+app.use(express.json());
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const port = process.env.PORT;
+const uri = process.env.URI;
+const { db, mongoose } = require("./db");
+db(uri).then(() => {
+  app.listen(port, () => {
+    console.log(`Congrats dev your app is runing ${port}`);
+  });
+});
+const route = require("./route");
+app.use("/", route);
